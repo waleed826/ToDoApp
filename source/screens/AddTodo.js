112d8fbcb82e245b0630899f7a54c1d00de47dbd
waleed826 <waleedbinakbar@gmail.com>
2023-12-05 17:10:05 +0500

    latest

--- source/screens/AddTodo.js
+++ source/screens/AddTodo.js
@@ -1,5 +1,5 @@
-import { Alert, StyleSheet, Text, View, Image,RefreshControl } from 'react-native'
-import React, { useState } from 'react'
+import { Alert, StyleSheet, Text, View, Image, RefreshControl } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Feather from 'react-native-vector-icons/Feather'
 import CustomInput from '../Components/CustomInput';
 import CustomButton from '../Components/CustomButton';
@@ -9,51 +9,60 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 
 const AddTodo = ({ navigation }) => {
     const [loading, setLoading] = useState(true)
-    const [first ,setFirst]=useState('')
-    const [second ,setSecond]=useState('')
-    const [third ,setThird]=useState('')
+    const [first, setFirst] = useState('')
+    const [second, setSecond] = useState('')
+    const [third, setThird] = useState('')
     const [event, setEvent] = useState([])
-   
-const saveButton = async () => {
-    const savedContact = [...event, { first, second, third }]
-    console.log("🚀 ~ file: Add.js:27 ~ saveButton ~ savedContact:", savedContact)
-    setEvent(savedContact)
-    // console.log(contact)
-    const jsonValue = await JSON.stringify(event);
-    await AsyncStorage.setItem('my-key', jsonValue);
-    console.log("🚀 ~ file: Add.js:38 ~ storeData ~ my:", jsonValue)
-    navigation.navigate('Dasboard')
-}
+    const getData = async () => {
+        const data = await AsyncStorage.getItem('my-key')
+        const contacts = JSON.parse(data)
+        setEvent(contacts)
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const saveButton = async () => {
+        const savedContact = [...event, { first, second, third }]
+        console.log("🚀 ~ file: Add.js:27 ~ saveButton ~ savedContact:", savedContact)
+
+        // console.log(contact)
+        const jsonValue = JSON.stringify(savedContact);
+        await AsyncStorage.setItem('my-key', jsonValue);
+        console.log("🚀 ~ file: Add.js:38 ~ storeData ~ my:", jsonValue)
+        setEvent(savedContact)
+        navigation.navigate('Dasboard')
+    }
 
     return (
-        <KeyboardAwareScrollView refreshControl={<RefreshControl onRefresh={saveButton} refreshing={loading} colors={["black"]}/>}>
-        <View style={{ backgroundColor: '#EDEDED', }}>
-            <View style={styles.circleOne}>
-            </View>
-            <View style={styles.circleTwo}>
-            </View>
-            <View style={{}}>
-                <View style={{ padding: 10 }}>
-                    <Feather name='arrow-left-circle' size={30} style={{}} onPress={() => navigation.replace('Dasboard')} />
+        <KeyboardAwareScrollView refreshControl={<RefreshControl onRefresh={saveButton} refreshing={loading} colors={["black"]} />}>
+            <View style={{ backgroundColor: '#EDEDED', }}>
+                <View style={styles.circleOne}>
                 </View>
-                <View style={{ alignItems: 'center', justifyContent: 'center', }}>
-                    <Text style={{ fontWeight: 500, fontSize: 20, color: 'black',padding:10 }}>
-                        Welcome Onboard!
-                    </Text>
-                    <Image source={require('../images/Girlboylaptop.png')} style={{ margin:10 }}/>
-                    <Text style={{ color: 'rgba(85, 132, 122, 0.97)', margin:10  }}>Add What your want to do later on..</Text>
+                <View style={styles.circleTwo}>
                 </View>
-            </View>
-            <View>
-                <CustomInput color={'white'} onChangeText={setFirst} />
-                <CustomInput color={'white'} onChangeText={setSecond} />
+                <View style={{}}>
+                    <View style={{ padding: 10 }}>
+                        <Feather name='arrow-left-circle' size={30} style={{}} onPress={() => navigation.replace('Dasboard')} />
+                    </View>
+                    <View style={{ alignItems: 'center', justifyContent: 'center', }}>
+                        <Text style={{ fontWeight: 500, fontSize: 20, color: 'black', padding: 10 }}>
+                            Welcome Onboard!
+                        </Text>
+                        <Image source={require('../images/Girlboylaptop.png')} style={{ margin: 10 }} />
+                        <Text style={{ color: 'rgba(85, 132, 122, 0.97)', margin: 10 }}>Add What your want to do later on..</Text>
+                    </View>
+                </View>
+                <View>
+                    <CustomInput color={'white'} onChangeText={setFirst} />
+                    <CustomInput color={'white'} onChangeText={setSecond} />
 
-                <CustomInput color={'white'} onChangeText={setThird} />
-                <CustomButton title={'Add to list'} color={'#55847A'} 
-                onPress={saveButton}
-                />
+                    <CustomInput color={'white'} onChangeText={setThird} />
+                    <CustomButton title={'Add to list'} color={'#55847A'}
+                        onPress={saveButton}
+                    />
+                </View>
             </View>
-        </View>
         </KeyboardAwareScrollView>
     )
 }
--- source/screens/DashboardScreen.js
+++ source/screens/DashboardScreen.js
@@ -1,29 +1,35 @@
-import { Image, StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native'
+import { Image, StyleSheet, Text, View, Dimensions, ScrollView, TouchableOpacity, ImageBackground } from 'react-native'
 import React, { useState, useEffect, useCallback } from 'react'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useFocusEffect } from '@react-navigation/native'
+import CheckBox from 'react-native-check-box'
 
 const DashboardScreen = ({ navigation }) => {
-    useEffect(()=>{
-      getContacts()
-  },[eventData])
-  // useFocusEffect(
-  //   useCallback(
-  //     () => {
-  //       getEvents()
-  //     },
-  //     [],
-  //   )
-
-  // ) 
+  useEffect(() => {
+    getContacts()
+  }, [])
+  useFocusEffect(
+    useCallback(
+      () => {
+        getContacts()
+      },
+      [],
+    )
+  )
+  const [show, setShow] = useState(null)
+  const Pressed = (index) => {
+    setShow(index)
+  }
   const [eventData, setEvents] = useState([])
-  const getContacts = async()=>{
+  const getContacts = async () => {
     const data = await AsyncStorage.getItem('my-key')
     const contacts = JSON.parse(data)
     setEvents(contacts)
     console.log("🚀 ~ file: Contacts.js:20 ~ getContacts ~ contacts:", contacts)
- }
+  }
+  const [toggleCheckBox, setToggleCheckBox] = useState(false)
+
   const { height, width } = Dimensions.get('screen')
   return (
     <ScrollView style={{ backgroundColor: '#EDEDED', zIndex: 1, flex: 1 }}>
@@ -32,34 +38,65 @@ const DashboardScreen = ({ navigation }) => {
       </View>
       <View style={styles.circleTwo}>
       </View>
-      <View style={{ backgroundColor: 'rgba(85, 132, 122, 0.82)', height: 250, zindex: 9, borderWidth: 1, borderColor: 'black', borderCurve: 'continuous', alignItems: 'center', justifyContent: 'center' }}>
+
+      <ImageBackground source={require('../images/Rectangle.png')}
+        style={{
+          //  backgroundColor: 'rgba(85, 132, 122, 0.82)', 
+          height: 250, zindex: 9, borderCurve: 'continuous', alignItems: 'center', justifyContent: 'center'
+        }}>
         <Image source={require('../images/handsome.png')} />
         <Text style={{ fontWeight: '500', fontSize: 20, color: 'black' }}>Welcome Fisayomi</Text>
-      </View>
-      <View>
-        <Image style={{ alignSelf: 'center', }} source={require('../images/bicycle.png')} />
+      </ImageBackground>
+      <View style={{}}>
+        <Image style={{ alignSelf: 'center',bottom:20,zIndex:8 }} source={require('../images/bicycle.png')} />
       </View>
       <View>
         <Text>TODO APP</Text>
       </View>
-      <View style={{ backgroundColor: 'white', width: width * 0.6, padding: 10, borderRadius: 30 }}>
+      <View style={{ backgroundColor: 'white', width: width * 0.9, padding: 10, borderRadius: 30,margin:15 }}>
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 10 }}>
           <Text>Daily Tasks.</Text>
           <AntDesign name='pluscircle' color='black' size={20} onPress={() => navigation.navigate('add todo')} />
         </View>
         <ScrollView style={{ margin: 10 }}>
-          <Text style={{ fontSize: 15, color: 'black' }}>Follow Oluwafisayomi.dev on Twitter.</Text>
-          <Text style={{ fontSize: 15, color: 'black' }}>Learn Figma by 4pm.</Text>
-          <Text style={{ fontSize: 15, color: 'black' }}>Coding at 9am.</Text>
-          <Text style={{ fontSize: 15, color: 'black' }}>Watch Mr Beasts Videos.</Text>
           {
             eventData?.map((item, index) => {
               return (
+                // <TouchableOpacity onPress={()=>Pressed(index)} key={item.index}>
                 <View key={item.index}>
-                  <Text style={{color:'black'}}>{item.first}</Text>
-                  <Text  style={{color:'black'}}>{item.second}</Text>
-                  <Text  style={{color:'black'}}>{item.third}</Text>
+                  <View style={styles.boxView}>
+                    <CheckBox
+                      style={styles.boxStyle}
+                      onClick={() => setToggleCheckBox(!toggleCheckBox)}
+                      isChecked={toggleCheckBox}
+                      checkedCheckBoxColor={'green'}
+                      checkBoxColor={'gray'}
+                    />
+                    <Text style={{ color: 'black' }}>{item.first}</Text>
+                  </View>
+                  <View style={styles.boxView}>
+                    <CheckBox
+                      style={styles.boxStyle}
+                      onClick={() => setToggleCheckBox(!toggleCheckBox)}
+                      isChecked={toggleCheckBox}
+                      checkedCheckBoxColor={'green'}
+                      checkBoxColor={'gray'}
+                    />
+                    <Text style={{ color: 'black' }}>{item.second}</Text>
+                  </View>
+                  <View style={styles.boxView}>
+                    <CheckBox
+                      style={styles.boxStyle}
+                      onClick={() => setToggleCheckBox(!toggleCheckBox)}
+                      isChecked={toggleCheckBox}
+                      checkedCheckBoxColor={'green'}
+                      checkBoxColor={'gray'}
+                    />
+                    <Text style={{ color: 'black' }}>{item.third}</Text>
+                  </View>
                 </View>
+                // {show === index ? <Text style={{ color: 'yellow' }}>SHown</Text> : null}
+                // </TouchableOpacity>
               )
             })
           }
@@ -93,4 +130,11 @@ const styles = StyleSheet.create({
     left: -120,
     zindex: 5
   },
+  boxStyle: {
+    padding: 10,
+  },
+  boxView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
 })
